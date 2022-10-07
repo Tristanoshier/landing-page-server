cfba6fdf57d67632c12deb904f8547c2ece84fdb
Tristanoshier <tsoshier@bsu.edu>
2022-10-07 18:33:25 -0400

    change featured project

--- controllers/site.controller.js
+++ controllers/site.controller.js
@@ -8,7 +8,7 @@ const {
 } = require('../models');
 
 const postFavoriteTitles = ['Experiencing Burnout', 'A flame my love, a frequency - Colleen', 'Moving from Indiana to NYC'];
-const projectFavoriteTitles = ['Code Databank', 'Sample shop for an artist', 'Portfolio website for a local hair stylist'];
+const projectFavoriteTitles = ['Code Databank', 'Sample shop for an artist', 'Portfolio website for college graduate'];
 
 // Posts
 router.get('/posts', (req, res) => {
